c55104d186701b27fea77e9940443cf92aaaf133
unknown <gitgayu2708@gmail.com>
2024-07-24 14:30:54 +0530

    add changes

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -18,6 +18,7 @@ export default function Login({setDetails}) {
         navi('/home');
     }else{
         console.log("Login unsuccessfull Kindly register");
+        window.alert("your are not registered ");
         navi('/');
     }
  }
